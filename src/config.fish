4165a9b3644ae38ab4abdebeccb9d2eb176e4331
Phil Crockett <contact@philcrockett.com>
2026-08-05 07:00:16 +0200

    mise shell integration

--- src/config.fish
+++ src/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     direnv hook fish | source
     starship init fish | source
+    mise activate fish | source
 end
